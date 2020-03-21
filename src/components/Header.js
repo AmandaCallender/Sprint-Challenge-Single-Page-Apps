7dcce32ff86bd4317ae158aea3f494985abcc866
AmandaCallender <amanda.callender@outlook.com>
2020-03-20 20:40:55 -0400

    MVP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <div className="mainNav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/characterlist">Character List</Link>
+          </li>
+          <li>
+            <Link to="/searchform">Search Form</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
